33af9bc5c6f4182d2c0f58f68a8c57f7325f5aeb
BenDeErit07 <BenDeErit27@gmail.com>
2021-03-19 10:52:39 +0800

    Third Commit

---
+++ src/numintergers/ThirdCommit.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numintergers;
+
+/**
+ *
+ * @author De Erit
+ */
+public class ThirdCommit {
+    
+    public void member2(){
+        int b;
+        System.out.println("I'm Benjamin N. De Erit");
+        System.out.println("From Moncada, Tarlac");
+           System.out.println("Part of this group");
+           System.out.println("\n");
+           System.out.println("This is our simple program");
+        
+    
+    }
+  
+    public static void main(String[] args) {
+       NumIntergers objint = new NumIntergers();
+       
+        objint.member2();
+          
+    }
+    
+}
