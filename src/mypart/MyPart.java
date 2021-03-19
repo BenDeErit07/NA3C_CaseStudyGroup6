13411381f7311e47dba382f53f6f23cfb683390c
Angelica <angelica.guts05@gmail.com>
2021-03-19 10:10:22 +0800

    Second Commit

---
+++ src/mypart/MyPart.java
@@ -0,0 +1,22 @@
+
+package mypart;
+
+public class MyPart {
+
+   public void member1(){
+        int b;
+        System.out.println("I'm Angelica Guitierrez");
+        System.out.println("From San Miguel Tarlac City");
+           System.out.println("Part of this group");
+           System.out.println("\n");
+           System.out.println("This is our simple program");
+        
+    
+    }
+    public static void main(String[] args) {
+         MyPart objint = new MyPart();
+       
+        objint.member1();
+    }
+    
+}
